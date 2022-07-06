b55d874dec65e3b3d9148c206b5ed0e90d0e894f
Thomas <thomas-hardy@live.fr>
2022-07-06 12:23:02 +0200

    connexion.php (marche pas je sais pas pourquoi), header.php pas fini, delete index.html (useless)

---
+++ connexion.php
@@ -0,0 +1,26 @@
+<?php include_once "header.php"; ?>
+
+<form method="POST">
+<input type="text" placeholder="mail" name=mail/>
+<input type="text" placeholder="mot de passe" name=motdepasse/>
+<button name="connexion">Connexion</button>
+</form>
+
+<?php
+
+if(isset($_POST['connexion']))
+{
+    $mail = $_POST["mail"];
+    $motdepasse = $_POST["motdepasse"];
+
+    $sql = "SELECT * FROM utilisateurs WHERE mail = ? AND motdepasse = ?";
+    $requete = $chat->prepare($sql);
+    $requete->execute(array($mail, $motdepasse));
+    $user = $requete->fetch();
+    var_dump($user);
+
+    
+}
+
+?>
+
---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+
+$dsn = 'mysql:dbname=chat;host=localhost';
+$user = 'root';
+$password= '';
+
+$chat = new PDO($dsn, $user, $password);
+
+?>
