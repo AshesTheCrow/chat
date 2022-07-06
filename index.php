14d4350b5d1100cfc8f90e2d9ad1fc49aa258055
Thomas <thomas-hardy@live.fr>
2022-07-06 09:22:27 +0200

    bootstrap + chatbox jquery vite fait via site tuto chat

--- index.php
+++ index.php
@@ -5,24 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Chat</h1>
 
-    <div id="wrapper">
-        <div id="menu">
-              <p class="welcome">Welcome, <b></b></p>
-              <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-              <div style="clear:both"></div>
-          </div>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Chat</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Inscription</a>
+        <a class="nav-link" href="#">Connexion</a>
+        <a class="nav-link disabled">Déconnexion</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="container">
+    <h1>Bienvenue sur le chat</h1>
+
+
            
           <div id="chatbox"></div>
-           
-          <form name="message" action="">
+           <div>
+              <form name="message" action="">
               <input name="usermsg" type="text" id="usermsg" size="63" />
               <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-          </form>
+              </form>
       </div>
+</div>
       <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
       <script type="text/javascript">
       // jQuery Document
@@ -30,6 +48,7 @@
        
       });
       </script>
+
       </body>
       </html>
 
