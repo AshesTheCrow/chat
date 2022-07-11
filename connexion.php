6ace93c69d388276063d06c615abe416ba139488
Philippe <philippe.kurz@gmail.com>
2022-07-11 10:02:09 +0200

    petites modif Philippe

--- connexion.php
+++ connexion.php
@@ -1,5 +1,4 @@
 <?php include_once "header.php"; ?>
-<?php include_once "nav.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,10 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <title>Document</title>
+
 </head>
 <body>
+    <?php include_once "nav.php"; ?>
     <div class="container">
         <div class="row g-3 mt-5">
             <form method="POST">
@@ -33,26 +34,29 @@ if(isset($_POST['connexion']))
     $mail = $_POST["mail"];
     $motdepasse = $_POST["motdepasse"];
 
-    $sql = "SELECT * FROM utilisateurs WHERE mail = ? AND motdepasse = ?";
+    $sql = "SELECT * FROM utilisateurs WHERE mail = ?";
     $requete = $chat->prepare($sql);
-    $requete->execute(array($mail, $motdepasse));
+    $requete->execute(array($mail));
     $user = $requete->fetch();
-    // var_dump($user);
-    // echo ($user);
 
-                if($user == false)
-            {
-            echo "bad logins";
-            }
-                else 
-            {
-    
-                $_SESSION["connecte"] = "oui";
-                  header("Location: index.php");
-            }
+    if(password_verify($motdepasse, $user["motdepasse"]))
+    {
+        $_SESSION["connecte"] = "oui";
+        $_SESSION["user"]["id"] = $user["id"];
+        $_SESSION["user"]["nom"] = $user["nom"];
+        $_SESSION["user"]["prenom"] = $user["prenom"];
+        $_SESSION["user"]["pseudo"] = $user["pseudo"];        
+        $_SESSION["user"]["mail"] = $user["mail"];        
+
+        header("Location: index.php");
+        exit();
+    }
+    else
+    {
+        echo("<p>Mauvais mot de passe</p>");
     }
 
 
+}
+
 ?>
-
-
--- inscription.php
+++ inscription.php
@@ -10,7 +10,7 @@ if(isset($_POST['inscription'])){
         ':nom' => $_POST['nom'],
         ':pseudo' => $_POST['pseudo'],
         ':mail' => $_POST['mail'],
-        ':motdepasse' => $_POST['motdepasse']
+        ':motdepasse' => password_hash($_POST['motdepasse'], PASSWORD_DEFAULT)
     )
     );
     header("Location: connexion.php");
@@ -28,51 +28,45 @@ if(isset($_POST['inscription'])){
     <title>inscription au chat</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</head>
-<style>
-    *{
-        box-sizing:border-box;
-    }
-    body{
-        padding:0;
-        margin:0;
-    }
-    .centre{
-        
-        margin: 15% auto;
-    }
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
-</style>
+    <style>
+        .centre{
+            margin: 15% auto;
+        }
+    </style>
+
+</head>
+
 <body>
     <?php
-    include_once "nav.php";
+        include_once "nav.php";
     ?>
     <div class="container centre">
-    <form method="POST">
-        <div class="row g-2">
-            <div class="col-md-4">
-                <input type="text" name="prenom" class="form-control" id="inputPrenom" placeholder="prenom">
+        <form method="POST">
+            <div class="row g-2">
+                <div class="col-md-4">
+                    <input type="text" name="prenom" class="form-control" id="inputPrenom" placeholder="prenom">
+                </div>
+                <div class="col-md-4">
+                    <input type="text" name="nom" class="form-control" id="inputNom" placeholder="nom">
+                </div>
+                <div class="col-md-4">
+                    <input type="text" name="pseudo" class="form-control" id="inputPseudo" placeholder="pseudo">
+                </div>
             </div>
-            <div class="col-md-4">
-                <input type="text" name="nom" class="form-control" id="inputNom" placeholder="nom">
+            <div class="row g-2 mt-3">
+                <div class="col-md-6">
+                    <input type="email" name="mail" class="form-control" id="inputEmail" placeholder="mail">
+                </div>
+                <div class="col-md-4">
+                    <input type="password" name="motdepasse" class="form-control" id="inputPassword" placeholder="password">
+                </div>
+                <div class="col-md-2">
+                    <button type="submit" name="inscription" class="btn btn-primary">S'inscrire</button>
+                </div>
             </div>
-            <div class="col-md-4">
-                <input type="text" name="pseudo" class="form-control" id="inputPseudo" placeholder="pseudo">
-            </div>
-        </div>
-        <div class="row g-2 mt-3">
-            <div class="col-md-6">
-                <input type="email" name="mail" class="form-control" id="inputEmail" placeholder="mail">
-            </div>
-            <div class="col-md-4">
-                <input type="password" name="motdepasse" class="form-control" id="inputPassword" placeholder="password">
-            </div>
-            <div class="col-md-2">
-                <button type="submit" name="inscription" class="btn btn-primary">S'inscrire</button>
-            </div>
-        </div>
-    </form>
+        </form>
     </div>
 </body>
 </html>
