09324e399668afa839321be752b78ebd14711b37
pierre Mbakop <p2mbakop@gmail.com>
2022-07-13 13:58:22 +0200

    ..

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php
 
-// $dsn = 'mysql:dbname=chattp;host=localhost';
-// $user = 'root';
-// $password= '';
+$dsn = 'mysql:dbname=chattp;host=localhost';
+$user = 'root';
+$password= '';
 
-$dsn = 'mysql:dbname=dbs8004344;host=db5009437195.hosting-data.io';
-$user = 'dbu2336936';
-$password= 'f7dM5CE6r';
+// $dsn = 'mysql:dbname=dbs8004344;host=db5009437195.hosting-data.io';
+// $user = 'dbu2336936';
+// $password= 'f7dM5CE6r';
 
 $chat = new PDO($dsn, $user, $password);
 
